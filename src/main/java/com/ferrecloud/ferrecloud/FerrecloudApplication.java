090e6532ccf9bd96245fece63410e9e32fd624ef
norx01 <juan_pinillos@hotmail.com>
2026-06-17 17:48:28 -0500

    Initial commit

---
+++ src/main/java/com/ferrecloud/ferrecloud/FerrecloudApplication.java
@@ -0,0 +1,13 @@
+package com.ferrecloud.ferrecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerrecloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerrecloudApplication.class, args);
+	}
+
+}
